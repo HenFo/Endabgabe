67b874efcfeb642367eaa2ecc5a37428ed0e32b0
Unknown <h_fock01@wwu.de>
2018-07-10 11:54:08 +0200

    Möglichkeit suchen zu aktivieren
    Suchen entweder direkt in der Karte oder über eine extra Suchleiste oder beides

--- BootstrapStudio/Map/assets/js/mappage.js
+++ BootstrapStudio/Map/assets/js/mappage.js
@@ -1,15 +1,3 @@
-//var osmurl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-//    osmattrib = '&copy; <a href="http://openstreetmap.org/copyright">openstreetmap</a> contributors',
-//    osm = l.tilelayer(osmurl, { maxzoom: 18, attribution: osmattrib }),
-//    map = new l.map('map', { center: new l.latlng(51.961, 7.618), zoom: 13 }),
-//    drawnitems = l.featuregroup().addto(map);
-//l.control.layers({
-//    'osm': osm.addto(map),
-//    "mapbox": l.tilelayer('pk.eyj1ijoizgltym9kdw1ibyisimeioijjampln2t4dxyxady2m2twotqzmxnocjc2in0.g9bjj267dr8rbxbbgi2fyq', {
-//        attribution: 'mapbox'
-//    })
-//})
-
 /**
  * @see https://stackoverflow.com/questions/37166172/mapbox-tiles-and-leafletjs
  */
@@ -17,11 +5,43 @@ var mapboxUrl = 'https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?
 var accessToken = 'pk.eyJ1IjoiZGltYm9kdW1ibyIsImEiOiJjamplN2t4dXYxaDY2M2twOTQzMXNocjc2In0.g9BJj267dR8RBxBBgi2fyQ';
 var attribution = "<i class='fa fa-copyright'></i> <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> <i class='fa fa-copyright'> <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>";
 
-var satellite = L.tileLayer(mapboxUrl, { id: 'satellite-streets-v9', attribution: attribution, maxZoom: 20, accessToken: accessToken }),
-    outdor = L.tileLayer(mapboxUrl, { id: 'outdoors-v9', attribution: attribution, maxZoom: 20, accessToken: accessToken });
+var satellite = L.tileLayer(mapboxUrl, { id: 'satellite-streets-v10', attribution: attribution, maxZoom: 20, accessToken: accessToken }),
+    outdor = L.tileLayer(mapboxUrl, { id: 'outdoors-v10', attribution: attribution, maxZoom: 20, accessToken: accessToken });
 
-var map = new L.Map('map', { center: new L.LatLng(51.9606649, 7.6261347), zoom: 13 });
-L.control.layers({
+var map = new L.Map('map', { center: new L.LatLng(51.9606649, 7.6261347), zoomControl: false, zoom: 13 });
+L.control.zoom({
+    position: 'topright'
+}).addTo(map);
+
+///**
+// * @see https://github.com/8to5Developer/leaflet-custom-searchbox
+// */
+//$(document).ready(function () {
+//    var searchboxControl = createSearchboxControl();
+//    var control = new searchboxControl({
+//        sidebarTitleText: 'Header',
+//        sidebarMenuItems: {
+//            Items: [
+//                { type: "link", name: "Link 1 (github.com)", href: "http://github.com", icon: "icon-local-carwash" },
+//                { type: "link", name: "Link 2 (google.com)", href: "http://google.com", icon: "icon-cloudy" },
+//                { type: "button", name: "Button 1", onclick: "alert('button 1 clicked !')", icon: "icon-potrait" },
+//                { type: "button", name: "Button 2", onclick: "button2_click();", icon: "icon-local-dining" },
+//                { type: "link", name: "Link 3 (stackoverflow.com)", href: 'http://stackoverflow.com', icon: "icon-bike" },
+//            ]
+//        }
+//    });
+//    control._searchfunctionCallBack = function (searchkeywords) {
+//        if (!searchkeywords) {
+//            searchkeywords = "The search call back is clicked !!"
+//        }
+//        alert(searchkeywords);
+//    }
+//    map.addControl(control);
+//});
+
+L.control.layers(null,{
     "outdoor": outdor.addTo(map),
     "satellite": satellite,
-}).addTo(map);
+}, { position: 'topright' }).addTo(map);
+map.addControl(new L.Control.Fullscreen().setPosition("topright"));
+
