eec9a9fce3a4b489564bfc144939c06adf842f1f
Unknown <h_fock01@wwu.de>
2018-09-02 16:06:46 +0200

    index.js

--- routes/index.js
+++ routes/index.js
@@ -6,52 +6,76 @@ router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/impressum', function (req, res) {
-    res.render('impressum', { title: 'Impressum' });
+router.get('/Mensen/index', function (req, res) {
+    res.render('Mensen', { title: 'Mensen in Muenster' });
 })
 
-router.get('/mappage', function (req, res) {
-    res.render('mappage', { title: 'Mappage' });
+router.get('/fachbereich/index', function (req, res) {
+    res.render('fachbereich', { title: 'Fachbereiche' });
 })
 
-/* POST to GeoJson Service */
-router.post('/GeoJson', function (req, res) {
-    // Set our internal DB variable
-    var db = req.db;
-    // Get our form values. These rely on the "name" attributes
-    var GeoObject = JSON.parse( req.body.geoJSON);
-    console.log(GeoObject);
-    var GeoName = req.body.name;
-    // Set our collection
-    var collection = db.get("GeoCollection");
-    // Submit to the DB
-    collection.insert({
-        "GeoJsonName": GeoName,
-        "GeoObject": GeoObject,
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // And forward to success page
-            //JL("mylogger").info("erfolgreich zur Datenbank hinzugefügt!");
-            res.redirect("mappage");
-        }
-    });
-});
+router.get('/institute/index', function (req, res) {
+    res.render('institute', { title: 'Institute' });
+})
 
-/* GET Geo Data. */
-router.get('/addToMap', function (req, res) {
-    var db = req.db;
-    var collection = db.get('GeoCollection');
-    console.log(req.query.name);
-    var layerName = req.query.name;
-    collection.find({ GeoJsonName: layerName }, {}, function (e, docs) {
-        console.log(docs);
-        res.send(docs);
-    });
-});
+router.get('/editMap/index', function (req, res) {
+    res.render('editMap', { title: 'Karte bearbeiten' });
+})
+
+router.get('/createFachbereich/fachbereich', function (req, res) {
+    res.render('createFachbereich', { title: 'Erstelle ein Fachbereich' });
+})
+
+router.get('/createInstitut/institute', function (req, res) {
+    res.render('createInstitut', { title: 'Erstelle ein Institut' });
+})
+
+router.get('/editFachbereich/fachbereich', function (req, res) {
+    res.render('createFachbereich', { title: 'Bearbeite ein Fachbereich' });
+})
+
+router.get('/editInstitut/institute', function (req, res) {
+    res.render('createInstitut', { title: 'Bearbeite ein Institut' });
+})
+
+///* POST to GeoJson Service */
+//router.post('/GeoJson', function (req, res) {
+//    // Set our internal DB variable
+//    var db = req.db;
+//    // Get our form values. These rely on the "name" attributes
+//    var GeoObject = JSON.parse( req.body.geoJSON);
+//    console.log(GeoObject);
+//    var GeoName = req.body.name;
+//    // Set our collection
+//    var collection = db.get("GeoCollection");
+//    // Submit to the DB
+//    collection.insert({
+//        "GeoJsonName": GeoName,
+//        "GeoObject": GeoObject,
+//    }, function (err, doc) {
+//        if (err) {
+//            // If it failed, return error
+//            res.send("There was a problem adding the information to the database.");
+//        }
+//        else {
+//            // And forward to success page
+//            //JL("mylogger").info("erfolgreich zur Datenbank hinzugefügt!");
+//            res.redirect("mappage");
+//        }
+//    });
+//});
+
+///* GET Geo Data. */
+//router.get('/addToMap', function (req, res) {
+//    var db = req.db;
+//    var collection = db.get('GeoCollection');
+//    console.log(req.query.name);
+//    var layerName = req.query.name;
+//    collection.find({ GeoJsonName: layerName }, {}, function (e, docs) {
+//        console.log(docs);
+//        res.send(docs);
+//    });
+//});
 
 
 module.exports = router;
